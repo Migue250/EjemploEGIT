7874b22c758ecc1e714b602e3b2148a3c48dfc32
Miguel <miguel.gordo.martinez@gmail.com>
2026-02-02 09:43:03 +0100

    segundo commit

--- src/Ejemploegit/EjemploEGIT.java
+++ src/Ejemploegit/EjemploEGIT.java
@@ -3,7 +3,7 @@ package Ejemploegit;
 public class EjemploEGIT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Ejemplo Git eclipse");
 
 	}
 
