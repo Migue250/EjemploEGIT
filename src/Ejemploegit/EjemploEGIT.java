407ca72d7cc0a788bf433be7f27ad219abfbc0bf
Miguel <miguel.gordo.martinez@gmail.com>
2026-02-09 09:02:27 +0100

    commit rama1

--- src/Ejemploegit/EjemploEGIT.java
+++ src/Ejemploegit/EjemploEGIT.java
@@ -5,6 +5,7 @@ public class EjemploEGIT {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo Git eclipse");
         System.out.println("commit");
+        System.out.println("Primer cambio rama1");
 	}
 
 }
